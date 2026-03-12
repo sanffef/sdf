48f07d2e2ac7e8a4dae4811c572cca88a399a17b
student <you@example.com>
2026-03-13 04:23:08 +0600

    a

--- ksa/photo.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { Photo } from './photo.entity';
-export declare class PhotoService {
-    private photoRepository;
-    constructor(photoRepository: Repository<Photo>);
-    findAll(): Promise<Photo[]>;
-}
